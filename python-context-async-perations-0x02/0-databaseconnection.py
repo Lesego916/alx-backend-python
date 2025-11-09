2bae6e0c6ea957e6e2d02a11cc9845779d071469
Lesego916 <segobuyisile@gmail.com>
2025-11-09 13:35:39 +0200

    PBE

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self, db_name):
+        self.db_name = db_name
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        return self.cursor
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.commit()
+        self.conn.close()
+
+# Usage
+with DatabaseConnection('users.db') as cursor:
+    cursor.execute("SELECT * FROM users")
+    results = cursor.fetchall()
+    print(results)
---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+class ExecuteQuery:
+    def __init__(self, db_name, query, params=None):
+        self.db_name = db_name
+        self.query = query
+        self.params = params or ()
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        self.cursor.execute(self.query, self.params)
+        self.results = self.cursor.fetchall()
+        return self.results
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.commit()
+        self.conn.close()
+
+# Usage
+query = "SELECT * FROM users WHERE age > ?"
+with ExecuteQuery('users.db', query, (25,)) as results:
+    print(results)
